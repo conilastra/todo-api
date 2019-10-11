53754cf3d35ed58dede16f704eab16e633e2d1d9
conilastra <cflastra@gmail.com>
2019-10-11 14:14:52 +0000

    cloned repository

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -36,6 +36,44 @@ def handle_person():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/users', methods=['POST', 'GET'])
+@app.route('/users/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def users(id=None):
+
+    if request.method == 'GET':
+        if id is not None:
+            user = User.query.get(id)
+            return jsonify(user.serialize()), 200
+        else:
+            users = User.query.all()
+            users = list(map(lambda x: x.serialize(), users))
+            return jsonify(users), 200
+
+    if request.method == 'POST':
+        user = User()
+        user.name = "Alguien"
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(user.serialize()), 200
+    if request.method == 'PUT':
+        user = User.query.get(id)
+        user.name = "Monica Geller"
+        db.session.commit()
+        return jsonify(user.serialize()), 200
+    if request.method == 'DELETE':
+        user = User.query.get(id)
+        user.delete()
+        db.session.commit()
+
+'''@app.route('/users/<username>', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def todolist(username):
+
+   if request.method == 'GET':
+        todo = Tasks.query.filter_by(username=username)
+        return jsonify(todo.serialize()), 200
+'''
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,6 +2,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(200), unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, default=False)
+    #user = db.relationship(User)
+    #user_id = db.Column(Integer, ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "label": self.label,
+            "done": self.done
+        }
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=False, nullable=False)
+    tasks = db.relationship('Task', backref='users', lazy=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
